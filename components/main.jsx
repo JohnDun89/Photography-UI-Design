a3942e888fcc6a4112f731e0cc9767566de45702
John Duncan <john@johnduncanphoto.com>
2018-03-27 15:29:22 +0100

    main component created

---
+++ components/main.jsx
@@ -0,0 +1,15 @@
+import  React  from "react";
+
+class  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default ;
