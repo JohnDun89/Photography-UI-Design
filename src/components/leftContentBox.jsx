7ffaa3bdb4c9f1108edbd69a5b4a95f5fb8164df
John Duncan <john@johnduncanphoto.com>
2018-03-29 15:03:23 +0100

    key added to on clicks

--- src/components/leftContentBox.jsx
+++ src/components/leftContentBox.jsx
@@ -11,9 +11,8 @@ class LeftContentBox extends React.Component {
             <div className="parent" id="left-content-box">
                 < MainArrows />
                 
-                
-                <h2 onClick={this.props.action}>Portfolio Two</h2>
-                <h2>Portfolio One</h2>
+                <h2 key={1} onClick={this.props.action}>Portfolio One</h2>
+                <h2 key={2} onClick={this.props.action}>Portfolio Two</h2>
             </div>
         );
     }
